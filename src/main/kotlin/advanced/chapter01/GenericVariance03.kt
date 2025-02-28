35fc695109b3a0371412608120f02fb56f83efde
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-01 01:30:00 +0900

    chapter01
    - GenericVariance03

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance03.kt
@@ -0,0 +1,24 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance03 {
+    private fun printProcessedNumber(transformation: (Int) -> Any) {
+        println(transformation(42))
+    }
+
+    fun main() {
+        val intToDouble: (Int) -> Number = { it.toDouble() }
+        printProcessedNumber(intToDouble)
+
+        val numberAsText: (Number) -> String = { it.toString() }
+        printProcessedNumber(numberAsText)
+
+        val identity: (Number) -> Number = { it }
+        printProcessedNumber(identity)
+
+        val numberToInt: (Number) -> Int = { it.toInt() }
+        printProcessedNumber(numberToInt)
+
+        val numberHash: (Any) -> Number = { it.hashCode() }
+        printProcessedNumber(numberHash)
+    }
+}
---
+++ src/test/kotlin/advanced/chapter01/GenericVariance03Test.kt
@@ -0,0 +1,11 @@
+package advanced.chapter01
+
+import me.bossm0n5t3r.advanced.chapter01.GenericVariance03
+import kotlin.test.Test
+
+class GenericVariance03Test {
+    @Test
+    fun main() {
+        GenericVariance03.main()
+    }
+}
