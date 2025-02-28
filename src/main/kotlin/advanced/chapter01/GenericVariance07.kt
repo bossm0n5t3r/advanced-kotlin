8cca3f5e3dc377bf3e08fe9f2766208b2e464959
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-01 01:49:00 +0900

    chapter01
    - GenericVariance07

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance07.kt
@@ -0,0 +1,13 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance07 {
+    sealed class Either<out L, out R>
+
+    data class Left<out L>(
+        val value: L,
+    ) : Either<L, Nothing>()
+
+    data class Right<out R>(
+        val value: R,
+    ) : Either<Nothing, R>()
+}
