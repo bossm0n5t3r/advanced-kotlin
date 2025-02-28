5b8a84e0e80f33a220acfe38fe6616f82bc4e0f5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-01 01:00:00 +0900

    chapter01
    - GenericVariance01

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance01.kt
@@ -0,0 +1,51 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance01 {
+    interface Animal {
+        fun pet()
+    }
+
+    class Cat(
+        private val name: String,
+    ) : Animal {
+        override fun pet() {
+            println("$name says Meow")
+        }
+    }
+
+    private fun petAnimals(animals: List<Animal>) {
+        for (animal in animals) {
+            animal.pet()
+        }
+    }
+
+    private fun petMutableAnimals(animals: MutableList<Animal>) {
+        for (animal in animals) {
+            animal.pet()
+        }
+    }
+
+    fun main() {
+        val cats: List<Cat> = listOf(Cat("Mruczek"), Cat("Puszek"))
+        petAnimals(cats) // Can I do that? Yes.
+
+        // https://github.com/JetBrains/kotlin/blob/master/libraries/stdlib/jvm/builtins/Collections.kt#L124C1-L173
+        // public actual interface List<out E> : Collection<E> {
+        // ...
+        // }
+
+        val mutableCats: MutableList<Cat> = mutableListOf(Cat("Mruczek"), Cat("Puszek"))
+        // petMutableAnimals(mutableCats) // Can I do that? No.
+
+        // Type mismatch.
+        //      Required:
+        //          MutableList<GenericVariance01.Animal>
+        //      Found:
+        //          MutableList<GenericVariance01.Cat>
+
+        // https://github.com/JetBrains/kotlin/blob/master/libraries/stdlib/jvm/builtins/Collections.kt#L179-L238
+        // public actual interface MutableList<E> : List<E>, MutableCollection<E> {
+        // ...
+        // }
+    }
+}
---
+++ src/test/kotlin/advanced/chapter01/GenericVariance01Test.kt
@@ -0,0 +1,11 @@
+package advanced.chapter01
+
+import me.bossm0n5t3r.advanced.chapter01.GenericVariance01
+import kotlin.test.Test
+
+class GenericVariance01Test {
+    @Test
+    fun main() {
+        GenericVariance01.main()
+    }
+}
