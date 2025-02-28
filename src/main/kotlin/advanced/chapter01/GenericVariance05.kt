e7b2d4b584e57b132f438c5c15165489f270368b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-01 01:39:00 +0900

    chapter01
    - GenericVariance05

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance05.kt
@@ -0,0 +1,22 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance05 {
+    sealed class LinkedList<out T>
+
+    data class Node<T>(
+        val head: T,
+        val tail: LinkedList<T>,
+    ) : LinkedList<T>()
+
+    data object Empty : LinkedList<Nothing>()
+
+    fun main() {
+        val strs = Node("A", Node("B", Empty))
+        val ints = Node(1, Node(2, Empty))
+        val empty: LinkedList<Char> = Empty
+
+        println(strs)
+        println(ints)
+        println(empty)
+    }
+}
---
+++ src/test/kotlin/advanced/chapter01/GenericVariance05Test.kt
@@ -0,0 +1,11 @@
+package advanced.chapter01
+
+import me.bossm0n5t3r.advanced.chapter01.GenericVariance05
+import kotlin.test.Test
+
+class GenericVariance05Test {
+    @Test
+    fun main() {
+        GenericVariance05.main()
+    }
+}
