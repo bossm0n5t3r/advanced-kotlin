20f31f99101a02d07c16132412461fc48cb7a0b8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-03 02:47:30 +0900

    chapter01
    - GenericVariance10

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance10.kt
@@ -0,0 +1,30 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance10 {
+    open class Car
+
+    interface Boat
+
+    class Amphibious :
+        Car(),
+        Boat
+
+    class Producer<out T>(
+        val factory: () -> T,
+    ) {
+        fun produce(): T = factory()
+    }
+
+    fun main() {
+        val producer: Producer<Amphibious> = Producer { Amphibious() }
+        val amphibious: Amphibious = producer.produce()
+        val boat: Boat = producer.produce()
+        val car: Car = producer.produce()
+
+        val boatProducer: Producer<Boat> = producer
+        val boat1: Boat = boatProducer.produce()
+
+        val carProducer: Producer<Car> = producer
+        val car2: Car = carProducer.produce()
+    }
+}
