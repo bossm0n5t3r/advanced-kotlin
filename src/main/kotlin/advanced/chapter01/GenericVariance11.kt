a883657594925462ba7f57e187149989ab118a10
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-03 03:25:00 +0900

    chapter01
    - GenericVariance11

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance11.kt
@@ -0,0 +1,11 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance11 {
+    class Box<in T>(
+        private val value: T,
+    ) {
+        private fun get(): T =
+            value
+                ?: error("Value not set")
+    }
+}
