328f9b9ac8476586da86d17724483faba187498c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-03 02:44:30 +0900

    chapter01
    - GenericVariance08

---
+++ src/main/kotlin/advanced/chapter01/GenericVariance08.kt
@@ -0,0 +1,30 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object GenericVariance08 {
+    open class Dog
+
+    class Puppy : Dog()
+
+    class Hound : Dog()
+
+    class Box<in T> {
+        private var value: T? = null
+
+        fun put(value: T) {
+            this.value = value
+        }
+    }
+
+    fun main() {
+        val dogBox = Box<Dog>()
+        dogBox.put(Dog())
+        dogBox.put(Puppy())
+        dogBox.put(Hound())
+
+        val puppyBox: Box<Puppy> = dogBox
+        puppyBox.put(Puppy())
+
+        val houndBox: Box<Hound> = dogBox
+        houndBox.put(Hound())
+    }
+}
