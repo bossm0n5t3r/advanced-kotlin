5dcfb00c5e2e9e09d1ea126dac0f267b12ed15d8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-10 00:47:00 +0900

    chapter01
    - Exercise02

---
+++ src/main/kotlin/advanced/chapter01/Exercise02.kt
@@ -0,0 +1,28 @@
+package me.bossm0n5t3r.advanced.chapter01
+
+object Exercise02 {
+    sealed class Response<out R, out E>
+
+    class Success<out R>(
+        val value: R,
+    ) : Response<R, Nothing>()
+
+    class Failure<out E>(
+        val error: E,
+    ) : Response<Nothing, E>()
+
+    fun usage() {
+        val rs1 = Success(1)
+        val rs2 = Success("ABC")
+        val re1 = Failure(Error())
+        val re2 = Failure("Error")
+
+        val rs3: Success<Number> = rs1
+        val rs4: Success<Any> = rs1
+        val re3: Failure<Throwable> = re1
+        val re4: Failure<Any> = re1
+
+        val r1: Response<Int, Throwable> = rs1
+        val r2: Response<Int, Throwable> = re1
+    }
+}
