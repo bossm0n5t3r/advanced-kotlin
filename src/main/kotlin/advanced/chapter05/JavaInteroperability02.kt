1b6090d15fd4817320915a180b47fc78e912ebbf
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-25 23:55:15 +0900

    chapter05

---
+++ src/main/kotlin/advanced/chapter05/JavaInteroperability02.kt
@@ -0,0 +1,12 @@
+package me.bossm0n5t3r.advanced.chapter05
+
+object JavaInteroperability02 {
+    fun multiply(
+        a: Int,
+        b: Int,
+    ) = a * b
+
+//    public final int multiply(int a, int b) {
+//        return a * b;
+//    }
+}
