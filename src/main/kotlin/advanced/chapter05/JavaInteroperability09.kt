0a94daa7e60503b078cf7e0c7f56bedb49b38d11
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-27 00:29:00 +0900

    chapter05

---
+++ src/main/kotlin/advanced/chapter05/JavaInteroperability09.kt
@@ -0,0 +1,22 @@
+package me.bossm0n5t3r.advanced.chapter05
+
+object JavaInteroperability09 {
+    class Box {
+        var name = ""
+    }
+
+//    public static final class Box {
+//        @NotNull
+//        private String name = "";
+//
+//        @NotNull
+//        public final String getName() {
+//            return this.name;
+//        }
+//
+//        public final void setName(@NotNull String var1) {
+//            Intrinsics.checkNotNullParameter(var1, "<set-?>");
+//            this.name = var1;
+//        }
+//    }
+}
