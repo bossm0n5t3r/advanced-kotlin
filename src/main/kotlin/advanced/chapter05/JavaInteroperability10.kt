2b1dbaa7864fefd3f5525275f83457b8f7e7dd51
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-03-27 00:31:00 +0900

    chapter05

---
+++ src/main/kotlin/advanced/chapter05/JavaInteroperability10.kt
@@ -0,0 +1,14 @@
+package me.bossm0n5t3r.advanced.chapter05
+
+object JavaInteroperability10 {
+    class Box {
+        @JvmField
+        var name = ""
+    }
+
+//    public static final class Box {
+//        @JvmField
+//        @NotNull
+//        public String name = "";
+//    }
+}
